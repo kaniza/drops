5d93494d92f88ee6a8d25eaa06461389331696fa
kaniza <kaneuchi@gmail.com>
2018-06-23 16:42:43 +0900

    Added icon, droppernode, etc...

--- Drops/ViewController.swift
+++ Drops/ViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import SpriteKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var ballRadiusSlider:UISlider!
     @IBOutlet var skView:SKView!
     var scene:DropScene!
     
@@ -10,6 +10,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.scene = DropScene.init(size: self.view.bounds.size)
         self.skView.presentScene(scene)
+        self.ballRadiusSlider.maximumValue = DropScene.maxBallSize
+        self.ballRadiusSlider.minimumValue = 3
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.ballRadiusSlider.isHidden = !UserDefaults.standard.bool(forKey: DropScene.showBallRadiusBarKey)
     }
 
     override func didReceiveMemoryWarning() {
@@ -18,6 +25,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func configViewDidEnd(segue: UIStoryboardSegue) {
+        self.scene.configChanged()
+        self.ballRadiusSlider.isHidden = !UserDefaults.standard.bool(forKey: DropScene.showBallRadiusBarKey)
+    }
+    
+    @IBAction func adjustBallRadiusSetting(sender: UISlider) {
+        UserDefaults.standard.set(sender.value, forKey:DropScene.ballRadiusKey)
         self.scene.configChanged()
     }
 }
